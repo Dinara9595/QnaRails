6c88b5dc66d2131c886cc33a6d42c5d840f0eedc
Dinara9595 <dinara.petuhova95@yandex.ru>
2022-03-30 11:32:51 +0300

    Tests controller Answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,4 @@
 class QuestionsController < ApplicationController
-  def index
-    @questions = Question.all
-  end
-
-  def show
-  end
 
   def create
     @question = Question.new(question_params)
@@ -16,32 +10,9 @@ class QuestionsController < ApplicationController
     end
   end
 
-  def update
-    if question.update(question_params)
-      redirect_to @question
-    else
-      render :edit
-    end
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def destroy
-    question.destroy
-    redirect_to questions_path
-  end
+  def new; end
 
   private
-
-  def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
-  end
-
-  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :answer do
     body { "MyText" }
+
+    trait :invalid do
+      body { nil }
+    end
+
+    association :question
   end
 end
